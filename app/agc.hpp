54f728878b5d0761f917e5379157bca5727fa060
Joel de Guzman <djowel@gmail.com>
2017-05-31 16:49:50 +0800

    Use integrator instead of low pass filter (integrators have a constant 90 deg. phase shift, which is desirable)

--- app/agc.hpp
+++ app/agc.hpp
@@ -16,19 +16,14 @@ namespace cycfi { namespace infinity
    //
    //    The signal chain is s follows:
    //
-   //       1) DC blocker: Blocks any DC offset from the signal.
-   //       2) Automatic filter control: Applies a one pole low pass filter 
-   //          with a dynamically adjusted cutoff frequency from half the 
-   //          sampling frequency down to 300Hz. The lower the signal level
-   //          (detected by the envelope follower), the lower the cutoff
-   //          frequency. This filters out high frequency noise present  
-   //          in low-level signals that will be amplified by a large factor
-   //          by the automatic gain control.
-   //       3) Envelope follower: Tracks the envelope of the signal. Take note
-   //          that this is exracted *after* the automatic filter control.
-   //       4) Noise gate: Suppresses the output when the signal drops to a
+   //       1) DC blocker: Blocks any DC offset from the original signal.
+   //       2) Integrator: Integrates the signal.
+   //       3) DC blocker: Blocks any DC offset from the integrated signal.
+   //       4) Envelope follower: Tracks the envelope of the signal. Take note
+   //          that this is exracted *after* integration.
+   //       5) Noise gate: Suppresses the output when the signal drops to a
    //          certain threshold.
-   //       5) Automatic gain control: Attempts to maintain a constant 
+   //       6) Automatic gain control: Attempts to maintain a constant 
    //          amplitude by dynamically applying gain from 1.0f to max_gain.
    //          The lower the signal level (detected by the envelope follower), 
    //          the higher the gain.
@@ -36,7 +31,7 @@ namespace cycfi { namespace infinity
    template <std::uint32_t sps>
    struct agc
    {  
-      static constexpr float max_gain = 500.0f;
+      static constexpr float max_gain = 100.0f;
       static constexpr float threshold = 1.0f / max_gain;
       static constexpr float low_freq = 1.0f - exp(-_2pi * 300.0f/sps);
       static constexpr float high_freq = 1.0f - exp(-_2pi * 0.5);
@@ -45,11 +40,10 @@ namespace cycfi { namespace infinity
       {      
          // DC block
          s = _dc_block(s);
-
-         // Automatic filter control
-         _low_pass.a = linear_interpolate(low_freq, high_freq, _env_follow());
-         s = _low_pass(s);
-
+         
+         // Integrate and second DC block
+         s = _dc_block2(_integrate(s));
+         
          // Envelope follower
          auto env = _env_follow(std::abs(s));
 
@@ -67,9 +61,10 @@ namespace cycfi { namespace infinity
       }
 
       schmitt_trigger _noise_gate = {/* hysteresis */ threshold * 0.5f};
-      envelope_follower _env_follow = {/* decay */ 0.001f};
-      one_pole_lp _low_pass = {0.0f};
-      dc_block _dc_block;
+      envelope_follower _env_follow = {/* decay */ 0.0005f};
+      dc_block _dc_block = { 10.0f /* hz */, sps };
+      dc_block _dc_block2 = { 10.0f /* hz */, sps };
+      integrator _integrate = {0.1};
    };
 }}
 
