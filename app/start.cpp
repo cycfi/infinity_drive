6eeb412cd0fc6993b7c9f5438b5ebc035ef9e5c2
djowel <djowel@gmail.com>
2017-11-18 17:55:18 +0800

    better synth tracking test

--- app/start.cpp
+++ app/start.cpp
@@ -22,13 +22,54 @@ using namespace inf::port;
 ///////////////////////////////////////////////////////////////////////////////
 // Our synthesizer
 constexpr uint32_t sps = 20000;
-bool note_on = true;
 
 auto synth = q::sin(110.0, sps);
 auto ref_synth = q::sin(150.0, sps, q::pi/4);
 
 using pls_type = inf::pls<decltype(ref_synth), sps, 100>;
 pls_type pls{ ref_synth };
+
+template <typename Synth>
+struct synth_gate
+{
+   enum { start, sustain, release, stop };
+
+   synth_gate(Synth& synth)
+    : _synth(synth) {}
+
+   float operator()()
+   {
+      if (state == start)
+      {
+         _synth.phase(0);
+         state = sustain;
+      }
+      else if (state == release)
+      {
+         if (_synth.is_start())
+            state = stop;
+      }
+      bool enable = (state == sustain || state == release);
+      return  enable? _synth() : 0.0f;
+   };
+
+   void note_on()
+   {
+      if (state == stop)
+         state = start;
+   }
+
+   void note_off()
+   {
+      if (state == sustain)
+         state = release;
+   }
+
+   Synth& _synth;
+   int state = stop;
+};
+
+synth_gate<decltype(synth)> sgate{ synth };
 
 ///////////////////////////////////////////////////////////////////////////////
 // Peripherals
@@ -45,7 +86,8 @@ void timer_task()
    // the signals to 0.8.
 
    static uint32_t sample_clock = 0;
-   auto val = note_on? synth() : 0.0f;
+
+   auto val = sgate();
    dac1((0.8f * val * 2047) + 2048);
    dac2((0.8f * pls(val, sample_clock++) * 2047) + 2048);
 }
@@ -57,7 +99,7 @@ constexpr uint32_t tmr_freq = 80000000;
 auto config = inf::config(
    dac1.setup(),
    dac2.setup(),
-   tmr.setup(tmr_freq, sps, timer_task)   // calls timer_task every 100kHz
+   tmr.setup(tmr_freq, sps, timer_task)   // calls timer_task every 20kHz
 );
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -65,6 +107,8 @@ auto config = inf::config(
 void start()
 {
    tmr.start();
+
+   sgate.note_on();
    inf::delay_ms(3000);
    auto start = q::phase::freq(80.0, sps);
    auto stop = q::phase::freq(1600.0, sps);
@@ -72,17 +116,17 @@ void start()
 ;
    while (true)
    {
-      note_on = false;
+      sgate.note_off();
       inf::delay_ms(500);
-      note_on = true;
+      sgate.note_on();
       synth.freq(start);
       while (synth.freq() < stop)
       {
          synth.freq(synth.freq() * step);
          inf::delay_ms(1000);
-         note_on = false;
+         sgate.note_off();
          inf::delay_ms(500);
-         note_on = true;
+         sgate.note_on();
       }
    }
 }
