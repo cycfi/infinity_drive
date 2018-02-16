9712015a5bbb9c17b85f64d67c57be263257fcba
djowel <djowel@gmail.com>
2018-02-16 10:54:43 +0800

    added delta filter

---
+++ app/delta_filter.hpp
@@ -0,0 +1,33 @@
+/*=============================================================================
+   Copyright (c) 2014-2017 Cycfi Research. All rights reserved.
+
+   Distributed under the MIT License [ https://opensource.org/licenses/MIT ]
+=============================================================================*/
+#if !defined(INFINITY_DELTA_FILTER_HPP_FEBRUARY_16_2018)
+#define INFINITY_DELTA_FILTER_HPP_FEBRUARY_16_2018
+
+#include <q/fx.hpp>
+#include <q/support.hpp>
+
+namespace cycfi { namespace infinity
+{
+   namespace q = cycfi::q;
+
+   struct delta_filter
+   {
+      delta_filter(float dfreq, uint32_t sps)
+       : _dlp(dfreq, sps)
+      {}
+
+      float operator()(float s)
+      {
+         auto dy = s - _y;
+         return _y += _dlp(dy);
+      }
+
+      float _y = 0.0f;
+      q::one_pole_lowpass _dlp;
+   };
+}}
+
+#endif
